ec19b0560b4bd4eccef7fdbf9ad6d0b8007fb6e4
Bartosz Kaszowski <bartosz@kaszowski.eu>
2022-01-02 22:09:02 +0100

    Design header

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        fontFamily: {
+            sans: ['"Open Sans"'],
+        },
+        extend: {
+            colors: {
+                "hotline": "#F9F9F9",
+                "hotlineBorder": "#F2F2F2",
+                primary: "#444444",
+                secondary: "#666666",
+            },
+            spacing: {
+                '45.5': '11.375rem',
+                '10.5': '3.75rem',
+            },
+            width: {
+                '128': '32rem',
+            },
+        },
+    },
+    plugins: [
+        function ({
+            addComponents
+        }) {
+            addComponents({
+                '.container': {
+                    maxWidth: '100%',
+                    '@screen sm': {
+                        maxWidth: '600px',
+                    },
+                    '@screen md': {
+                        maxWidth: '768px',
+                    },
+                    '@screen lg': {
+                        maxWidth: '992px',
+                    },
+                    '@screen xl': {
+                        maxWidth: '1170px',
+                    },
+                }
+            })
+        },
+    ],
+}
